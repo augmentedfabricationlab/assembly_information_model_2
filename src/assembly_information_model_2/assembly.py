b7ebe41b97f2d15cebbe0b71d662b51084627938
Kathrin Dörfler <doerfler.kathrin@gmail.com>
2024-02-24 23:44:52 +0100

    example

--- src/assembly_information_model_2/assembly.py
+++ src/assembly_information_model_2/assembly.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from copy import deepcopy
+
 from compas.datastructures import Assembly as AssemblyCompas
 
 class Assembly(AssemblyCompas):
@@ -29,4 +31,48 @@ class Assembly(AssemblyCompas):
 
     def __init__(self, name=None, **kwargs):
         super(Assembly, self).__init__()
+    
+    def __str__(self):
+        tpl = "This assembly contains {} parts and {} connections."
+        return tpl.format(self.graph.number_of_nodes(), self.graph.number_of_edges())
+
+    def transform(self, T):
+        """Transforms this assembly.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+
+        Returns
+        -------
+        None
+        """
+        for part in self.parts():
+            part.transform(T)
+
+    def transformed(self, T):
+        """Returns a transformed copy of this assembly.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+
+        Returns
+        -------
+        Assembly
+        """
+        assembly = self.copy()
+        assembly.transform(T)
+        return assembly
+    
+    def copy(self):
+        """Returns a copy of this assembly.
+        """
+        cls = type(self)
+        return cls.__from_data__(deepcopy(self.__data__))
+
+    def part(self, key):
+        """Get a part by its key."""
+
+        return self.graph.node[key]['part']
 
